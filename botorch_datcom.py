c0f6f5a23c35c887ab57a28cd103c1374666f9c3
Bugra Eryilmaz <70061581+BugraEryilmaz@users.noreply.github.com>
2021-08-17 10:31:13 +0300

    datcom optimization loop is constructed

--- botorch_datcom.py
+++ botorch_datcom.py
@@ -1,8 +1,41 @@
+# %% Hyperparameters
+NUMBER_OF_INIT_POINTS = 5
+NUMBER_OF_ITERATIONS = 100
+
 # %% DATCOM env
 import gym
 import datcom_gym_env
 env = gym.make('Datcom-v1')
 
+# %% Model definition
+class SimpleCustomGP(ExactGP, GPyTorchModel):
+
+    _num_outputs = 1  # to inform GPyTorchModel API
+    
+    def __init__(self, train_X, train_Y):
+        # squeeze output dim before passing train_Y to ExactGP
+        super().__init__(train_X, train_Y.squeeze(-1), GaussianLikelihood())
+        self.mean_module = ConstantMean()
+        self.covar_module = ScaleKernel(
+            base_kernel=RBFKernel(ard_num_dims=train_X.shape[-1]),
+        )
+        self.to(train_X)  # make sure we're on the right device/dtype
+        
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return MultivariateNormal(mean_x, covar_x)
+
+# %% Fit the model
+from botorch.fit import fit_gpytorch_model
+
+def _get_and_fit_simple_custom_gp(Xs, Ys, **kwargs):
+    model = SimpleCustomGP(Xs[0], Ys[0])
+    mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    fit_gpytorch_model(mll)
+    return model
+
+# %% DATCOM evaluate    
 # parameters = XLE1, XLE2, CHORD1_1, CHORD1_2, CHORD2_1, CHORD2_2, SSPAN1_2, SSPAN2_2
 def evaluate_param(XLE1, XLE2, CHORD1_1, CHORD1_2, CHORD2_1, CHORD2_2, SSPAN1_2, SSPAN2_2):
     defaultXLE1, maxXLE1, minXLE1 = 1.72, 1.75, 1.25
@@ -44,13 +77,12 @@ def evaluate_param(XLE1, XLE2, CHORD1_1, CHORD1_2, CHORD2_1, CHORD2_2, SSPAN1_2,
     
     return gain
 
-# %%
 def datcom_eval(parameterization, *args):
     XLE1, XLE2, CHORD1_1, CHORD1_2, CHORD2_1, CHORD2_2, SSPAN1_2, SSPAN2_2 = parameterization["XLE1"], \
         parameterization["XLE2"], parameterization["CHORD1_1"], parameterization["CHORD1_2"], parameterization["CHORD2_1"], \
         parameterization["CHORD2_2"], parameterization["SSPAN1_2"], parameterization["SSPAN2_2"]
     gain = evaluate_param(XLE1, XLE2, CHORD1_1, CHORD1_2, CHORD2_1, CHORD2_2, SSPAN1_2, SSPAN2_2)
-    return {"datcom_res": (gain, 0.0)}
+    return {"objective": (gain, 0.0)}
     
 # %%
 from ax import ParameterType, RangeParameter, SearchSpace
@@ -91,6 +123,35 @@ datcom_exp = SimpleExperiment(
     name="test_datcom",
     search_space=search_space_datcom,
     evaluation_function=datcom_eval,
-    objective_name="datcom_res",
+    objective_name="objective",
     minimize=False,
 )
+
+# %% Get initial random points
+from ax.modelbridge import get_sobol
+
+sobol = get_sobol(exp.search_space)
+exp.new_batch_trial(generator_run=sobol.gen(NUMBER_OF_INIT_POINTS))
+
+# %% Optimization loop & results
+from ax.modelbridge.factory import get_botorch
+
+for i in range(NUMBER_OF_ITERATIONS):
+    print(f"Running optimization batch {i+1}/{NUMBER_OF_ITERATIONS}...")
+    model = get_botorch(
+        experiment=exp,
+        data=exp.eval(),
+        search_space=exp.search_space,
+        model_constructor=_get_and_fit_simple_custom_gp,
+    )
+    batch = exp.new_trial(generator_run=model.gen(1))
+    
+print("Done!")
+
+# get the index of minimum value
+idxmin = exp.eval().df['mean'].idxmin()
+# get the arm name and value at the minimum index
+arm_name, optimum_val = exp.eval().df.iloc[idxmin,0], exp.eval().df.iloc[idxmin,2]
+# get the parameters for the minimum output
+optimum_param = exp.arms_by_name[arm_name].parameters
+print(optimum_param, optimum_val)
